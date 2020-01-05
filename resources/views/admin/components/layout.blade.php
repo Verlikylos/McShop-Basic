925394b2e970e7e256b5c157047d81ab8557869f
Kamil Twardowski <kamil.twardowsky@gmail.com>
2020-01-05 16:30:21 +0100

    Admin Control Panel views and Dashboard controller with it's view

---
+++ resources/views/admin/components/layout.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="stylesheet" href="{{ mix('/css/themes/vmcshop.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/datatables.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/fontawesome.css') }}">
+    
+    <title>vMCShop Basic - @yield('title')</title>
+</head>
+<body class="acp">
+
+<header class="shadow-sm">
+    <div class="logo-wrapper">
+        <img class="img-fluid mx-auto" src="{{ asset('images/logo.png') }}" alt="Page Logo">
+    </div>
+</header>
+
+<div class="container position-relative">
+    <div class="acp-raised-card shadow">
+        @if (isset($withoutSidebar) && $withoutSidebar)
+            <main>
+                @yield('content')
+            </main>
+        @else
+            <div class="row">
+                
+                @include('admin.components.sidebar')
+                
+                <main class="col-12 col-md-9 col-lg-10">
+                    <div class="acp-card-header">
+                        @yield('acp-card-title')
+                    </div>
+                    
+                    @if (session('sessionMessage'))
+                        <div class="alert {{ 'alert-' . session('sessionMessage')['type'] }} alert-with-icon alert-dismissible fade show" role="alert">
+                            <div class="alert-icon">
+                                @if (session('sessionMessage')['type'] === 'success')
+                                    <i class="fas fa-check fa-fw"></i>
+                                @elseif (session('sessionMessage') === 'danger')
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                @else
+                                    <i class="fas fa-info-circle"></i>
+                                @endif
+                            </div>
+                            <p class="alert-message">{!! session('sessionMessage')['content'] !!}</p>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    @endif
+                    
+                    @yield('content')
+                </main>
+            </div>
+        @endif
+    </div>
+    
+    <footer>
+        <a href="{{ route('home') }}">
+            <i class="fas fa-arrow-left"></i> Przejdź do sklepu
+        </a>
+        <span class="copyright">
+            Proudly powered by <a href="https://vmcshop.pro">vMCShop Basic</a> {{ env('VMCSHOP_VERSION') }}
+        </span>
+    </footer>
+</div>
+
+<script src="{{ mix('/js/bootstrap.js') }}"></script>
+<script src="{{ asset('/js/datatables.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,84 @@
+@extends('admin.components.layout')
+
+@section('acp-card-title')
+    <h4 class="acp-card-title">
+        <i class="fas fa-desktop"></i> Pulpit
+    </h4>
+@endsection
+
+@section('content')
+    <div class="row">
+        <div class="col-12 col-md-7">
+            <div class="card bg-white border-primary shadow-sm">
+                <div class="card-header text-white bg-primary">
+                    <i class="fas fa-chart-line"></i> Sprzedaż usług w miesiącu Listopad 2019
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-5">
+            <div class="card bg-white border-primary shadow-sm">
+                <div class="card-header text-white bg-primary">
+                    <i class="fas fa-chart-bar"></i> Sprzedaż usług w roku 2019
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-12 col-md-6 col-lg-6">
+            <div class="card bg-white border-primary shadow-sm">
+                <div class="card-header text-white bg-primary">
+                    <i class="fas fa-server"></i> Status serwerów
+                </div>
+                <div class="card-body">
+                    <table id="acpServerStatusesTable" class="table table-responsive-lg">
+                        <tbody>
+                        <tr>
+                            <td>Serwer Hardcore</td>
+                            <td>
+                                <span class="badge badge-success">Online</span>
+                            </td>
+                            <td class="text-right">
+                                <span class="badge badge-info">25/100</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Serwer Survival</td>
+                            <td>
+                                <span class="badge badge-success">Online</span>
+                            </td>
+                            <td class="text-right">
+                                <span class="badge badge-info">25/100</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Serwer MiniGames</td>
+                            <td>
+                                <span class="badge badge-danger">Offline</span>
+                            </td>
+                            <td class="text-right">
+                                <span class="badge badge-info">0/0</span>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="card bg-white border-primary shadow-sm">
+                <div class="card-header text-white bg-primary">
+                    Informacje o skrypcie
+                </div>
+                <div class="card-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
