36bbb9fa45fcda642689ea95851d6050f37924d6
Kamil Twardowski <kamil.twardowsky@gmail.com>
2020-01-05 16:40:03 +0100

    User database seeder with starter admin account

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'password' => bcrypt('password'),
+            'avatar_url' => asset('images/default-avatar.png'),
+            'permissions' => '[]',
+        ]);
+    }
+}
