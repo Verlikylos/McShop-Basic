c5014a064f5e9cbe392b94be5458b9b3600f6ca3
Kamil Twardowski <kamil.twardowsky@gmail.com>
2020-01-19 20:55:33 +0100

    Add icon to every setting tab

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -11,7 +11,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Ogólne</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-tools"></i>
+                    Ogólne
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">
                     Przejdź
@@ -23,7 +26,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Płatności</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-money-bill-wave"></i>
+                    Płatności
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">
                     Przejdź
@@ -35,7 +41,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Numery SMS</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-sms"></i>
+                    Numery SMS
+                </h5>
                 <p class="card-text">Możesz skonfigurować tu numery SMS Premium dla wybranych operatorów płatności.</p>
                 <a href="{{ route('admin.settings.numbers.index') }}" class="btn btn-primary">
                     Przejdź
@@ -47,7 +56,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Vouchery</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-ticket-alt"></i>
+                    Vouchery
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">
                     Przejdź
@@ -59,7 +71,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Wygląd</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-object-group"></i>
+                    Wygląd
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">
                     Przejdź
@@ -71,7 +86,10 @@
     <div class="col-12 col-md-6 col-lg-4 mb-3">
         <div class="card settings-card">
             <div class="card-body">
-                <h5 class="card-title font-weight-bold">Inne</h5>
+                <h5 class="card-title font-weight-bold">
+                    <i class="fas fa-sliders-h"></i>
+                    Inne
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="btn btn-primary">
                     Przejdź
